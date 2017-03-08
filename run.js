380b866ac4cae5501a9ff5c45ec26759bd8712c2
kenpeter <figo2476@hotmail.com>
2017-03-08 23:47:10 +1100

    working

--- run.js
+++ run.js
@@ -3,7 +3,8 @@ var http = require('http');
 var cheerio = require('cheerio');
 var fs = require('fs');
 
-var mkdirp = require('mkdirp');  
+var mkdirp = require('mkdirp');
+const util = require('util');
 var urls = [];
 
 var pornArr = [
@@ -12,32 +13,72 @@ var pornArr = [
 
 function getHtml(href) {
   var pageData = '';
-  
+
   var req = http.get(href, function(res) {
     res.setEncoding('utf8');
     res.on('data', function(chunk) {
       pageData += chunk;
     });
-    
+
     res.on('end', function() {
-    
+
       //console.log(pageData);
-      $ = cheerio.load(pageData);
-      var html = $("img");
+      // https://stackoverflow.com/questions/24565800/how-do-get-script-content-using-cheerio
+      $ = cheerio.load(pageData, {xmlMode: false});
+      var html = $('script').get();
 
-      console.log(html);
+      // https://stackoverflow.com/questions/10729276/how-can-i-get-the-full-object-in-node-jss-console-log-rather-than-object
+      //test
 
+      //console.log(util.inspect(html, false, 4));
       // http://img-egc.xvideos.com/videos/thumbs169ll/68/17/0a/68170a4bfbf6ed0252fad43362ddad03/mozaiquehome.jpg
       for(var i = 0; i < html.length; i++) {
-        var src = html[i].attribs.src;
-        
-        /*
-        // not https, http
-        if (src.indexOf("http://img-egc.xvideos.com/") > -1) {
-          
+        var childrenArr = html[i].children;
+        if(childrenArr.length > 0) {
+          let mydata = html[i].children[0].data;
+          // https://stackoverflow.com/questions/37645945/how-to-get-image-url-from-html-string-using-regex
+          let re = /<img\b(?=\s)(?=(?:[^>=]|='[^']*'|="[^"]*"|=[^'"][^\s>]*)*?\ssrc=['"]([^"]*)['"]?)(?:[^>=]|='[^']*'|="[^"]*"|=[^'"\s]*)*"\s?\/?>/;
+          let matches = mydata.match(re);
+          if(matches !== null) {
+            //test
+            //console.log("--- start ---");
+            //console.log(matches[1]);
+            //console.log("--- end ---");
+            //console.log();
+            // small: http://img-egc.xvideos.com/videos/thumbs169/0a/d3/32/0ad332719d82ae16ed318269b131574c/0ad332719d82ae16ed318269b131574c.4.jpg
+            // bigger: http://img-egc.xvideos.com/videos/thumbs169ll/0a/d3/32/0ad332719d82ae16ed318269b131574c/0ad332719d82ae16ed318269b131574c.4.jpg
+            let myurl = matches[1];
+            let arr = myurl.split("/");
+            /*
+            [ 'videos',
+              'thumbs169',
+              'bc',
+              '5d',
+              '02',
+              'bc5d02a383ea63ac9a1dbf05fd093103',
+              'bc5d02a383ea63ac9a1dbf05fd093103.1.jpg' ]
+            */
+
+            let newMyurl =
+              'http://img-egc.xvideos.com/' +
+              arr[3] + "/" +
+              arr[4] + "ll/" +
+              arr[5] + "/" +
+              arr[6] + "/" +
+              arr[7] + "/" +
+              arr[8] + "/" +
+              arr[9];
+
+            //console.log(newMyurl);
+            urls.push(newMyurl);
+          }
+          else {
+
+          }
         }
-        */
-        urls.push(html[i].attribs.src);
+        else {
+          continue;
+        }
       }
 
       if (true) {
@@ -70,19 +111,19 @@ function downImg(imgurl) {
   http.get(tmpImgUrl, function(res) {
     var imgData = "";
 		// need binary, otherwise cannot see img
-    res.setEncoding("binary"); 
+    res.setEncoding("binary");
 
 		// res on data
-		// 
+		//
     res.on("data", function(chunk) {
       imgData += chunk;
     });
-   
-		// end 
+
+		// end
     res.on("end", function() {
-      console.log("");
-      console.log(narr);
-    
+      //console.log("");
+      //console.log(narr);
+
 			// buld the path we want
       var savePath = __dirname + "/upload/tmp/" + narr[5];
       // now write
@@ -104,15 +145,15 @@ function downImg(imgurl) {
 }
 
 
-// 
+//
 function start() {
   console.log("--- starting ---");
-  
+
   // https://stackoverflow.com/questions/21194934/node-how-to-create-a-directory-if-doesnt-exist
   mkdirp(__dirname + '/upload/tmp', function (err) {
     if (err) console.error(err);
     else console.log('dir created!')
-    
+
     // get it
     for (var i=0; i < pornArr.length; i++) {
       getHtml(pornArr[i]);
